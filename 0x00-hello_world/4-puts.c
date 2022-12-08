d4686cc10231881f94425d59a9adc7b1635691a5
Sekani-Kahlil <Judef06@gmail.com>
2022-12-08 11:15:12 -0800

    using the puts func

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - main block
+ * return: o
+ */
 int main(void){
 
-	    puts("Programming is like building a multilingual puzzle\n");
-
+	    puts("\"Programming is like building a multilingual puzzle");
 	        return 0;
-
 }
