00cc4b82f5d2ee02ad4dda7aea104031ecac095a
Sekani-Kahlil <Judef06@gmail.com>
2022-12-10 14:12:17 -0800

    standard error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Return: Always 0 (success)
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("size of a char: %lu byte(s)\n", sizeof(char));
-	printf("size of an int: %lu byte(s)\n", sizeof(int));
-	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("size of a float: %lu byte(s)\n", sizeof(float));
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
-}
+}	
