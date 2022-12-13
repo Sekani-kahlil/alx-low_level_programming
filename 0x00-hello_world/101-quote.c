dd7d8c2266fcdc175666641bb63a613cafbbea8a
Sekani-Kahlil <Judef06@gmail.com>
2022-12-12 22:02:38 -0800

    operating system UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
-#include <unistd.h>
-
-/**
- * main - prints exactly "and that piece of art is useful" - Dora Korpar,2015-10-19",
- * followed by a new line, to standard error.
- * Return: Always 1.*
- */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
