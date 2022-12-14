ae2fabc63a582525f65d2bc4dd1b220b23ba55bb
Sekani-Kahlil <Judef06@gmail.com>
2022-12-13 23:56:07 -0800

    4_alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if a number is lowercase
+ * @c: the integer value it receiveis
+ * Return: 1 if true. 0 if false
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
