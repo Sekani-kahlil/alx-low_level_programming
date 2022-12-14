e7b9a37e2779a6820b764e1aac64c2ea54a2b7f9
Sekani-Kahlil <Judef06@gmail.com>
2022-12-13 17:12:20 -0800

    alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
-int putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
