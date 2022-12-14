1422f2dcedc3e82b23ade8d90e2c27189720ae5c
Sekani-Kahlil <Judef06@gmail.com>
2022-12-13 23:41:22 -0800

    is_slower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - check if a number is lowercase
+ * @c: the integer value it receiveis
+ * Return: 1 if true. 0 if false
+ */
+
+int _islower(int c)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	if (c == i)
+	{
+	return (1);
+	}
+	}
+	return (0);
+}
