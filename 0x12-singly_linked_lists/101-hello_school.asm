8d0196915b5cb4f13675129a76419b4e5193f6ac
Sekani-Kahlil <Judef06@gmail.com>
2023-01-26 13:11:07 -0800

    101-hello_school.asm

--- 0x12-singly_linked_lists/101-hello_school.asm
+++ 0x12-singly_linked_lists/101-hello_school.asm
@@ -1,15 +1,21 @@
-	SECTION .data
+extern printf
+
+section .data
 msg:	db "Hello, Holberton", 0
 fmt:	db "%s", 10, 0
 
-	SECTION .text
+section .text
 	extern printf
 	global main
 main:
+	push pbp
+
 	mov esi, msg
 	mov edi, fmt
 	mov eax, 0
 	call printf
+
+	pop pbp
 
 	mov eax, 0
 	ret
